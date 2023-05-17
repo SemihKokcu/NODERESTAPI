3b3f7fcf1ec70b82702ac02c125f1c1b02705238
SemihKokcu <semihkkc1@gmail.com>
2023-05-17 15:05:12 +0300

    get posts and single post

--- routes/feed.js
+++ routes/feed.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { body } = require('express-validator');
+const { body } = require('express-validator/check');
 
 const feedController = require('../controllers/feed');
 
@@ -14,12 +14,14 @@ router.post(
   [
     body('title')
       .trim()
-      .isLength({ min: 5 }),
+      .isLength({ min: 7 }),
     body('content')
       .trim()
       .isLength({ min: 5 })
   ],
   feedController.createPost
 );
+
+router.get('/post/:postId', feedController.getPost);
 
 module.exports = router;
