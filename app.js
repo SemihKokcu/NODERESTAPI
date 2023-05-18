77cc87f4e5d47eb8c8d23f5f9d275e85a6eff81e
SemihKokcu <semihkkc1@gmail.com>
2023-05-18 15:00:25 +0300

    socket io added and used post operations

--- app.js
+++ app.js
@@ -64,6 +64,10 @@ mongoose
     'mongodb+srv://semih:315312@atlascluster.cap2wer.mongodb.net/messages?retryWrites=true'
   )
   .then(result => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require('./socket').init(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
   })
   .catch(err => console.log(err));
